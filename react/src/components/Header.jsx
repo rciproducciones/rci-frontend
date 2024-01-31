b4c233b3c1d4d1f0b44a702e5bb8b6c0e3a818af
Júlia Sendra <juliasendra98@gmail.com>
2024-01-31 17:45:22 +0100

    atomize footer and header components from homepage

---
+++ react/src/components/Header.jsx
@@ -0,0 +1,69 @@
+import "../css/Header.css";
+import logorci from "../assets/images/logorci.png";
+
+function Header() {
+    return(
+        <>
+        <div className="logocontainer">
+        <img className="logorci" src={logorci} alt="Logo RCI" />
+      </div>
+
+      <div className="navbar bg-zinc-50 flex justify-center">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-zinc-50 rounded-box w-52"
+          >
+            <li>
+              <a>Inicio</a>
+            </li>
+            <li>
+              <a>Películas</a>
+            </li>
+            <li>
+              <a>Nosotras</a>
+            </li>
+            <li>
+              <a>Contacto</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Inicio</a>
+            </li>
+            <li>
+              <a>Películas</a>
+            </li>
+            <li>
+              <a>Nosotras</a>
+            </li>
+            <li>
+              <a>Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      </>
+    )
+}
+
+export default Header;
--- react/src/pages/HomePage.jsx
+++ react/src/pages/HomePage.jsx
@@ -1,90 +1,21 @@
 import "../css/App.css";
 import "../css/HomePage.css";
-import logorci from "../assets/images/logorci.png";
+import Header from "../components/Header";
 import Carousel from "../components/Carousel";
-import CatalogPage from "../components/CatalogPage"
+import CatalogPage from "../components/CatalogPage";
+import Footer from "../components/Footer";
 
 function HomePage() {
   return (
     <>
-    <>
-    <Carousel></Carousel>
-    </>
-      <div className="logocontainer">
-        <img className="logorci" src={logorci} alt="Logo RCI" />
+      <>
+        <Carousel></Carousel>
+      </>
+      <Header></Header>
+      <div>
+        <CatalogPage></CatalogPage>
       </div>
-
-      <div className="navbar bg-zinc-50 flex justify-center">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-zinc-50 rounded-box w-52"
-          >
-            <li>
-              <a>Inicio</a>
-            </li>
-            <li>
-              <a>Películas</a>
-            </li>
-            <li>
-              <a>Nosotras</a>
-            </li>
-            <li>
-              <a>Contacto</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Inicio</a>
-            </li>
-            <li>
-              <a>Películas</a>
-            </li>
-            <li>
-              <a>Nosotras</a>
-            </li>
-            <li>
-              <a>Contacto</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-  <div>
-<CatalogPage></CatalogPage>
-  </div>
-      <footer className="footer footer-center p-10 bg-zinc-600 text-primary-content">
-  <aside>
-    <p>© 2024 RCI Producciones | Murcia</p>
-    <p>Aviso Legal y Política de Privacidad | Política de Cookies</p>
-  </aside> 
-  <nav>
-    <div className="grid grid-flow-col gap-4">
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
-    </div>
-  </nav>
-</footer>
-
+      <Footer></Footer>
     </>
   );
 }
