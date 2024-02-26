043f0bfb38e851def598242fb6039f750d94c5ed
Júlia Sendra <juliasendra98@gmail.com>
2024-02-26 17:32:40 +0100

    Visual changes in Contact page & Footer

--- react/src/components/ContactForm.jsx
+++ react/src/components/ContactForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "../css/Contact.css";
-import { FaPhone, FaMapMarkerAlt } from "react-icons/fa";
+
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -27,8 +27,10 @@ const ContactForm = () => {
 
   return (
     <div>
-      <h1 className="title1 text-3xl font-bold mb-4">Amamos las historias. ¡Cuéntanos la tuya!</h1>
-      <div className="main-container flex h-full">
+      <div className="main-container">
+            <h1 className="title1 text-3xl font-bold">Amamos las historias.</h1>
+            <h1 className="title2 text-3xl font-bold">¡Cuéntanos la tuya!</h1>
+            
         <div className="contact-form-container items-center">
           <form
             onSubmit={handleSubmit}
@@ -126,35 +128,6 @@ const ContactForm = () => {
             </button>
           </form>
           </div>
-
-          <div className="location-container">
-            <div className="location">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3144.7221798741098!2d-1.129565323537498!3d37.98361240010181!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd638205353bda9f%3A0xd68f756203c739e5!2sC.%20Pintor%20Villacis%2C%204%2C%2030003%20Murcia!5e0!3m2!1ses!2ses!4v1708103389435!5m2!1ses!2ses"
-                width="600"
-                height="500"
-                style={{ border: "0" }}
-                allowFullScreen=""
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-              ></iframe>
-            </div>
-            <div className="contact-info">
-              <p>
-                <FaPhone className="inline-block mr-2" />
-                <a href="tel:+34630374129">+34 630 37 41 29</a>
-              </p>
-              <p>
-                <FaMapMarkerAlt className="inline-block mr-2" />
-                C/ Pintor Villacis, 4, PP OF.11
-              </p>
-              <p>
-                <FaMapMarkerAlt className="inline-block mr-2" />
-                Murcia, 30003
-              </p>
-            
-          </div>
-        </div>
       </div>
     </div>
   );
